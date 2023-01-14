ce553e34547ad8b512bc37904e87f4fc1db8af80
Marcio Alberto De Oliveira Lourenco <marcio.lourenco@proway.treina>
2023-01-13 21:40:56 -0300

    atualizacoes templates

--- Python/aula 11-01/alunos/views.py
+++ Python/aula 11-01/alunos/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    alunos = {
+        1:'Marcio',
+        2:'Haiko',
+        3:'Larissa'
+    }
+    dados = {
+        'nome_do_aluno':alunos
+    }
+    return render(request, 'index.html', dados)
 
 def aluno(request):
     return render(request, 'aluno.html')
