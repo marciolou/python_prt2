7819188a25bfd66af3aed37f4bcd309b48a77d39
marciolou <marcioalbertoo.lourenco@gmail.com>
2023-01-29 08:17:37 -0300

    corrigindo bugs

--- Python/aula 26-01/models.py
+++ Python/aula 26-01/models.py
@@ -2,7 +2,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 class Funcionarios(BaseModel):
-    id: int[Optional] = None
+    id: Optional[int] = None
     nome: str
     idade: int
     email: str
