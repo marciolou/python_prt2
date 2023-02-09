65d0f8a92c78e96c22539861f3b9e9d3696a93d9
Marcio Alberto De Oliveira Lourenco <marcio.lourenco@proway.treina>
2023-02-09 19:57:45 -0300

    Upando atualizações

--- Python/aula 02_03/api/v1/api.py
+++ Python/aula 02_03/api/v1/api.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from api.v1.endpoints import aluno
+from api.v1.endpoints import usuario
+
 
 api_router = APIRouter()
 
 api_router.include_router(aluno.router, prefix='/alunos', tags=['alunos'])
-api_router.include_router(aluno.router, prefix='/usuarios', tags=['usuarios'])
+api_router.include_router(usuario.router, prefix='/usuarios', tags=['usuarios'])
--- Python/aula 02_03/core/configs.py
+++ Python/aula 02_03/core/configs.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://teste:123456@localhost:5433/postgres'
+    DB_URL: str = 'postgresql+asyncpg://teste:123456@192.168.0.53:5439/postgres'
     DB_BaseModel = declarative_base()
 
     class Config:
